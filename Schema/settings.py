13cbe382a03ec3b6830720f1dc49f4667982b258
zyy1611 <2213958880@qq.com>
2021-09-10 15:25:44 +0800

    adjust windowsize again

--- Schema/settings.py
+++ Schema/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=ewp)i_8g-i@s3bogdfgmp1m+i2#n)ng6qwb%4%nh0-j0fb)6@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
